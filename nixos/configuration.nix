276a2c9a390fb0d724a04661607fbb19596478a3
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-02-18 17:56:04 -0500

    WIP

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,12 +15,12 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   # From the full installer
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   # From the generated config
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
