25246f55098e568f2b679337326b3a089e876b7c
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-02-20 15:31:46 -0500

    Add minimal-iso/flake.nix file with NixOS installation media configuration

---
+++ minimal-iso/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Minimal NixOS installation media";
+  inputs.nixos.url = "nixpkgs/23.11-beta";
+  outputs = { self, nixos }: {
+    nixosConfigurations = {
+      exampleIso = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, ... }: {
+              environment.systemPackages = with pkgs; [
+                wget
+                curl
+                htop
+                emacs-nox
+                git
+              ];
+
+          })
+        ];
+      };
+    };
+  };
+}
