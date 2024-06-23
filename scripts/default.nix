ec4a7fdd9f90eff69787ed136ddd4a9d73113b61
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-06-22 22:11:40 -0400

    chore: Update installation command for nixos-disko-format-install

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,17 +2,22 @@
 
 stdenv.mkDerivation {
   pname = "nixos-disko-format-install";
-  version = "1.0";  # Consider specifying a version
+  version = "1.0"; # Consider specifying a version
 
   src = ./nixos-disko-format-install.sh;
 
-  buildInputs = [ jq gum ];  # Dependencies
+  # Prevent Nix from trying to unpack the script
+  dontUnpack = true;
 
-# Directly specify the installation command in one line
-  installPhase = "install -D -m0755 ${./nixos-disko-format-install.sh} $out/bin/nixos-disko-format-install";
+  # Directly specify the installation command
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${./nixos-disko-format-install.sh} $out/bin/nixos-disko-format-install
+    chmod +x $out/bin/nixos-disko-format-install
+  '';
 
   meta = {
     description = "A script to format disks with disko and install NixOS";
-    license = lib.licenses.mit;  # Adjust the license as appropriate
+    license = lib.licenses.mit; # Adjust the license as appropriate
   };
 }
